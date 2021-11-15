935a11e1cb7c2fb289bfb3e0419b7bfe18f7949b
jseo123 <jose123135@gmail.com>
2021-11-15 13:31:55 +0100

    Added functionality to move months around

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const weekdays = ["Sunday", "Monday", "Tuesday", "Wenesday", "Thursday", "Friday
 
 function load() {
     const dt = new Date();
+    if(nav !==0) {
+        dt.setMonth(new Date().getMonth() + nav);
+    }
 
     const day = dt.getDate();
     const month = dt.getMonth();
@@ -25,6 +28,8 @@ function load() {
     const paddingDays = weekdays.indexOf(dateString.split(", ")[0]);
     document.getElementById("monthDisplay").innerText = dt.toLocaleDateString("en-GB", {month: "long"})+ " " + year;
 
+calendar.innerHTML = "";
+
     for(let i = 1; i <= paddingDays + daysInMonth; i++) {
         const daySquare = document.createElement("div");
         daySquare.classList.add("day");
@@ -41,5 +46,20 @@ function load() {
     }
     console.log(paddingDays)
 }
+
+function initButtons() {
+document.getElementById("nextButton").addEventListener("click", () => {
+nav++;
+load();
+});
+
+document.getElementById("backButton").addEventListener("click", () => {
+    nav--;
+    load();
+    });
+
+}
+
+initButtons();
 
 load();
