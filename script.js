17fbf0b24e677f28286225c48436e3732cf1e9d3
jseo123 <jose123135@gmail.com>
2021-11-17 10:51:19 +0100

    Changed the format of padding days

--- script.js
+++ script.js
@@ -30,6 +30,8 @@ function load() {
         dt.setMonth(new Date().getMonth() + nav);
     }
 
+  console.log(Date)
+
     const day = dt.getDate();
     const month = dt.getMonth();
     const year = dt.getFullYear();
@@ -60,7 +62,7 @@ calendar.innerHTML = "";
         const daySquare = document.createElement("div");
         daySquare.classList.add("day");
 
-        const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+        const dayString = `${i - paddingDays}/${month + 1}/${year}`;
 
         if (i > paddingDays) {
             daySquare.innerText = i - paddingDays;
